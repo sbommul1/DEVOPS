4f941b8c88a9f1941845e0c2912dcc85b8ef0c3c
sbommul1 <subrahmanya.saikrishna@gmail.com>
2025-06-11 08:35:20 -0700

    Terraform directory

--- terraform/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "aws_instance_public_ip" {
-    value = aws_instance.my-terraform-server.public_ip
-}
--- terraform/variables.tf
+++
@@ -1,6 +0,0 @@
-variable "region" {
-    description = "region name"  
-    default = "eu-north-1"
-    type = string
-    
-}
